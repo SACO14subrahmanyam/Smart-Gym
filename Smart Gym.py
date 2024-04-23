6d86e1f1c31cabee47ec9d1659d853875021e770
Penujuli Subrahmanyam <96356756+SACO14subrahmanyam@users.noreply.github.com>
2024-04-23 14:33:37 +0530

    Add files via upload

---
+++ Smart Gym.py
@@ -0,0 +1,139 @@
+import cv2
+import face_recognition
+import numpy as np
+import os
+import mediapipe as mp
+from datetime import datetime
+
+mp_drawing = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+
+# Load face recognition images
+path ='Camera Roll'
+images= []
+classNames=[]
+mylist =os.listdir(path)
+for cl in mylist:
+    curImg=cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+
+def findEncodings(images):
+    encodelist=[]
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodelist.append(encode)
+    return encodelist
+def markAttendence(name):
+    with open('Attendence.csv','r+') as f:
+        myDatalist = f.readlines()
+        nameList=[]
+        for line in myDatalist:
+            entry = line.split(',')
+            nameList.append(entry[0])
+            if name not in nameList:
+                now=datetime.now()
+                dtString=now.strftime('%H:%H:%S')
+                f.writelines(f'\n{name},{dtString}')
+
+
+encodelistknown=findEncodings(images)
+print('Encoding complete')
+
+# Setup camera
+cap=cv2.VideoCapture(0)
+
+def calculate_angle(a, b, c):
+    a = np.array(a)
+    b = np.array(b)
+    c = np.array(c)
+
+    radians = np.arctan2(c[1] - b[1], c[0] - b[0]) - np.arctan2(a[1] - b[1], a[0] - b[0])
+    angle = np.abs(radians * 180.0 / np.pi)
+    if angle > 180.0:
+        angle = 360 - angle
+
+    return angle
+
+# Curl counter variables
+counter = 0
+stage = None
+
+# Setup mediapipe instance
+with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Face recognition
+        small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
+        small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
+        face_locations = face_recognition.face_locations(small_frame)
+        face_encodings = face_recognition.face_encodings(small_frame, face_locations)
+
+        for encodeFace, faceLoc in zip(face_encodings, face_locations):
+            matches = face_recognition.compare_faces(encodelistknown, encodeFace)
+            faceDis = face_recognition.face_distance(encodelistknown, encodeFace)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                y1, x2, y2, x1 = faceLoc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(frame, (x1, y2 - 35), (x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(frame, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+                markAttendence(name)
+
+        # Pose detection
+        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = pose.process(image)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        try:
+            landmarks = results.pose_landmarks.landmark
+
+            shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x,
+                        landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+            elbow = [landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_ELBOW.value].y]
+            wrist = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x,
+                     landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+
+            angle = calculate_angle(shoulder, elbow, wrist)
+
+            cv2.putText(image, str(angle),
+                        tuple(np.multiply(elbow, [640, 480]).astype(int)),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2, cv2.LINE_AA
+                        )
+
+            if angle > 160:
+                stage = "down"
+            if angle < 30 and stage == 'down':
+                stage = "up"
+                counter += 1
+                print(counter)
+
+        except Exception as e:
+            print(e)
+
+        # Render detections
+        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
+                                  mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=2),
+                                  mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                                  )
+
+        # Display counter
+        cv2.putText(image, 'REPS', (15, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, str(counter), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+        cv2.putText(image, 'STAGE', (65, 12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0), 1, cv2.LINE_AA)
+        cv2.putText(image, stage, (60, 60), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+
+        cv2.imshow('Combined Feed', image)
+
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
